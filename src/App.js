95f4a1d39db075bfda18f9654790827a424f13bc
sada498 <sada.y@outlook.com>
2019-10-13 11:22:58 +1100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,16 +5,11 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a>
+      
           this code changes in test environment.
           <h1> Test branch edit </h1>
-        </a>
-      </header>
+      
+      
     </div>
   );
 }
